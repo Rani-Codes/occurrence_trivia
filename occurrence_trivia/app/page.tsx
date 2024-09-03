4c74af85f07bac89d40b7924031b0c0998984182
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-09-02 23:58:42 -0700

    created initial card layout

--- occurrence_trivia/app/page.tsx
+++ occurrence_trivia/app/page.tsx
@@ -1,8 +1,10 @@
+import Card from "@/components/card";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
+      <Card/>
     </main>
   );
 }
---
+++ occurrence_trivia/components/card.tsx
@@ -0,0 +1,13 @@
+const Card = () => {
+  return (
+    <div className="w-full flex justify-center">
+        <div className="flex justify-center w-4/12 h-96 rounded-lg bg-blackOlive">
+            <div className="flex justify-center items-center text-floralWhite">
+                Card
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Card
