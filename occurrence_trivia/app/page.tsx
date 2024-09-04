b38d7b0e4c62cce33cff35c2537c8ffb0c940009
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-09-03 23:58:35 -0700

    added slider component to card

--- occurrence_trivia/app/page.tsx
+++ occurrence_trivia/app/page.tsx
@@ -1,10 +1,14 @@
 import Card from "@/components/card";
+import CostumSlider from "@/components/slider"
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
       <Card/>
+      <div className="flex justify-center w-full text-eerieBlack">
+        <CostumSlider/>
+      </div>
     </main>
   );
 }
---
+++ occurrence_trivia/components/slider.tsx
@@ -0,0 +1,65 @@
+'use client'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import Typography from '@mui/material/Typography';
+
+const MAX = 12;
+const MIN = 1;
+const marks = [
+  {
+    value: MIN,
+    label: '',
+  },
+  {
+    value: MAX,
+    label: '',
+  },
+];
+
+export default function CustomSlider() {
+  const [val, setVal] = React.useState<number>(MIN);
+  const handleChange = (_: Event, newValue: number | number[]) => {
+    setVal(newValue as number);
+  };
+
+  return (
+    <Box sx={{ width: 3/12 }}>
+      <Slider
+        marks={marks}
+        step={1}
+        value={val}
+        valueLabelDisplay="auto"
+        min={MIN}
+        max={MAX}
+        onChange={handleChange}
+        sx={{
+            color: '#eb5e28ff',
+            '& .MuiSlider-thumb': {
+              backgroundColor: '#eb5e28ff', // Color of the thumb
+            },
+            '& .MuiSlider-track': {
+              backgroundColor: '#eb5e28ff', // Color of the track
+            },
+
+          }}
+      />
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography
+          variant="body2"
+          onClick={() => setVal(MIN)}
+          sx={{ cursor: 'pointer' }}
+        >
+          January
+        </Typography>
+        <Typography
+          variant="body2"
+          onClick={() => setVal(MAX)}
+          sx={{ cursor: 'pointer' }}
+        >
+          December
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
